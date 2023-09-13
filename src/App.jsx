74437b5ee2e85a4aedbd55fbc946062b4e78b39b
Gabriel-Tabarelli <gabriel_tabarelli@estudante.sc.senai.br>
2023-09-12 21:42:05 -0300

    Refatorando tudo

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,33 @@
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
-import { useState, useRef, useEffect } from 'react';
 
-function App() {
+const App = () => {
 
-  const nome = "henrique"
-  let [nomeDigitado, setNomeDigitado] = useState('');
-
-  const inputRef = useRef([]);
-  const inputs = nome.split('');
-
-  useEffect(() => {
-    setNomeDigitado('');
-    inputRef.current[0].focus();
-  }, [])
-
-  const nextInput = (e, index) => {
-    if (e.key === 'Backspace') {
-      if (index > 0 && e.target.value.length === 0) {
-        inputRef.current[index - 1].focus();
-      }
-      setNomeDigitado(nomeDigitado.slice(0, -1));
-      return;
-    }
-    setNomeDigitado(nomeDigitado + e.key);
-    if (index < inputs.length - 1 && e.target.value.length === 1) {
-      inputRef.current[index + 1].focus();
-      ganhou();
-      return;
-    }
-  }
-
-  useEffect(() => {
-    console.log(nomeDigitado)
-  }, [nomeDigitado])
-
-  const ganhou = () => {
+  const [palavra, setPalavra] = useState('');
+  const inputs = useRef([]);
     
-    if (nomeDigitado === nome) {
-      alert('Você ganhou!');
+  useEffect(() => {
+    setPalavra('teste');
+  }, []);
+
+  const handlerChange = (e, index) => {
+    console.log(e.target.value);
+    const { value } = e.target;
+    if (value.length !== 0) {
+      console.log('a')
     }
   }
 
   return (
     <>
       <div className='flex items-center justify-center w-full mt-20 space-x-4'>
-        {inputs.map((input, index) => (
-          <input type="text"
+        {palavra.split('').map((input, index) => (
+          <input 
+            type="text"
             maxLength="1"
+            ref={el => inputs.current[index] = el}
             key={index}
-            ref={input => { inputRef.current[index] = input }}
-            onKeyDown={e => nextInput(e, index)}
+            onKeyDown={(e) => handlerChange(e, index)}
             className='h-20 w-20 rounded-md border-0 text-gray-900 ring-1 ring-inset 
           ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600
           text-center text-4xl' />
